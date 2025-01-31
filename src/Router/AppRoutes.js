c0076b1b9a18dd59bb2b2d50e61b46a496b48f1e
Abdullah7498 <abdullahtanveer7429@gmail.com>
2025-02-01 00:27:39 +0500

    Add user fetching functionality with async thunk and clean up routing imports

--- src/Router/AppRoutes.js
+++ src/Router/AppRoutes.js
@@ -2,8 +2,6 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import USERS from "../layouts/USERS";
-// import Home from "../pages/Home";
-// import Dashboard from "../pages/Dashboard";
 
 function AppRoutes() {
   return (
--- src/store/slices/ChatSlice.js
+++ src/store/slices/ChatSlice.js
@@ -1,4 +1,22 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const fetchUsers = createAsyncThunk(
+  "users/fetchUsers",
+  async (queryParams = {}, { rejectWithValue }) => {
+    try {
+      const response = await fetch(
+        `/api/users?${new URLSearchParams(queryParams)}`
+      );
+      if (!response.ok) {
+        throw new Error("Failed to fetch users");
+      }
+      const data = await response.json();
+      return data.data;
+    } catch (err) {
+      return rejectWithValue(err.message);
+    }
+  }
+);
 
 const initialState = {
   chatID: null,
